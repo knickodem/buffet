4a10a605c2bb7d8cebd5b987332ef11a9e5544a0
Kyle Nickodem <kyle.nickodem@gmail.com>
2022-12-11 14:09:50 -0500

    Update scale_score.R

--- R/scale_score.R
+++ R/scale_score.R
@@ -2,69 +2,49 @@
 #'
 #' Combine item responses into a scale score using a sum, mean, or count
 #'
-#' @param df
-#' @param items character vector of column names in \code{df} to use in the scale
-#' @param type character indicating the type of score
-#' @inheritParams base::sum
-#' @param all.na.missing logical. When na.rm = TRUE, should rows with all NA
-#' still be treated as missing? Default is \code{FALSE}.
+#' @param data a \code{data.frame}
+#' @param items character vector of column names in \code{data} to use in the scale
+#' @param type should the score be the sum or the mean of item responses?
+#' @param min.valid the minimum number of valid responses to receive a score.
 #'
 #' @return numeric vector
 #'
 #' @examples
 #' set.seed(9876)
-#' df <- data.frame(x1 = sample(0:4, 20, replace = TRUE),
+#' data <- data.frame(x1 = sample(0:4, 20, replace = TRUE),
 #'                  x2 = sample(0:4, 20, replace = TRUE),
 #'                  x3 = sample(0:4, 20, replace = TRUE),
 #'                  x4 = sample(0:4, 20, replace = TRUE))
 #'
-#' sum.score <- scale_score(df, paste0("x", 1:4), type = "sum")
+#' sum.score <- scale_score(data, paste0("x", 1:4), type = "sum")
 #'
 #' ## Incorporate missing data
-#' dfmiss <- apply(df, 2, function(x){
+#' dfmiss <- apply(data, 2, function(x){
 #'                 x[sample(c(1:nrow(df)), floor(nrow(df)/10))] <- NA
 #'                 x})
 #' dfmiss <- as.data.frame(dfmiss)
 #'
-#' # count number of items with a response
-#' count.score <- scale_score(dfmiss, paste0("x", 1:4), type = "count", na.rm = TRUE)
-#'
-#' # Only score NA if NA for all items
-#' mean.score <- scale_score(df, paste0("x", 1:4), type = "mean",
-#'                           na.rm = TRUE, all.na.missing = TRUE)
+#' # Score NA if missing on more than 1 item
+#' mean.score <- scale_score(dfmiss, paste0("x", 1:4), type = "mean", min.valid = 3)
 #'
 #' @export
+scale_score <- function(data, items, type = c("sum", "mean"), min.valid = NULL){
 
-scale_score <- function(df, items,
-                        type = c("sum","count","mean"),
-                        na.rm = FALSE, all.na.missing = FALSE){
 
-  if(type %in% c("sum", "mean")){
-
-    s <- rowSums(df[,items], na.rm = na.rm)
-    score <- s
-
-  } else if(type %in% c("count", "mean")){
-
-    c <- rowSums(!is.na(df[,items]), na.rm = na.rm)
-    score <- c
-
+  if(type == "sum"){
+    score <- rowSums(data[, items], na.rm = TRUE)
+  } else if(type == "mean"){
+    score <- rowMeans(data[, items], na.rm = TRUE)
   }
 
-  if(type == "mean"){
+  nacount <- rowSums(is.na(data[, items]), na.rm = TRUE)
 
-    score <- s / c
-
-  }
-
-  if(na.rm == TRUE & all.na.missing == TRUE){
-
-    nacount <- rowSums(is.na(df[,items]), na.rm = TRUE)
+  if(!is.null(min.valid)){
+    score <- ifelse((length(items) - nacount) < min.valid, NA, score)
+  } else {
     score <- ifelse(nacount == length(items), NA, score)
-
   }
 
   return(score)
-
 }
 
